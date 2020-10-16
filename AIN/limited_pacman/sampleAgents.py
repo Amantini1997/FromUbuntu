196125f44c5c6bdbd69cc0c6c3ba857e729c585b
Amantini1997 <ale.amantini1997@gmail.com>
2020-10-16 23:21:11 +0200

    PACMAN WORKS HOLY SHITgit add .!

--- AIN/limited_pacman/sampleAgents.py
+++ AIN/limited_pacman/sampleAgents.py
@@ -33,9 +33,9 @@ import random
 import game
 import util
 
-import numpy as np
 import itertools
 from collections import defaultdict
+import operator
 
 # RandomAgent
 #
@@ -290,26 +290,52 @@ def manhattanNotAbsoluteDistance( xy1, xy2 ):
     return ( xy1[0] - xy2[0], xy1[1] - xy2[1] )
 
 
-def getBestNextMoves(legal, closestPointDistancesXY):
+def getBestNextMoves(legal, closestPointDistancesXY, immediateBestMoveFlag = False, maxDistanceForImmediateBestMove = 1):
     '''
     Given the legal moves and the closes point to pacman,
     Calculates and return the best moves Direction wise
-    as an array
+    as an array. If best move is best move, return True
+    at the end of the array
     '''
     EAST_DISTANCE = closestPointDistancesXY[0]
     NORTH_DISTANCE = closestPointDistancesXY[1]
 
     bestMoves = []
+    # immediate move NORTH SOUTH
+    if EAST_DISTANCE == 0 and abs(NORTH_DISTANCE) <= maxDistanceForImmediateBestMove: 
+        if Directions.NORTH in legal and NORTH_DISTANCE > 0:
+            bestMoves.append(Directions.NORTH)
+        elif Directions.SOUTH in legal and NORTH_DISTANCE < 0:
+            bestMoves.append(Directions.SOUTH)
+
+    # immediate move WEST EAST
+    if NORTH_DISTANCE == 0 and abs(EAST_DISTANCE) <= maxDistanceForImmediateBestMove: 
+        if Directions.EAST in legal and EAST_DISTANCE > 0:
+            bestMoves.append(Directions.EAST)
+        elif Directions.WEST in legal and EAST_DISTANCE < 0:
+            bestMoves.append(Directions.WEST)
+    
+    # immediate move exists
+    if len(bestMoves) > 0:
+        if immediateBestMoveFlag: bestMoves.append(True)
+        return bestMoves
+
+
+    bestMoves = []
     # x-axis move
     if EAST_DISTANCE >= 0 and Directions.EAST in legal:
         bestMoves.append(Directions.EAST)
+
     elif EAST_DISTANCE <= 0 and Directions.WEST in legal:
         bestMoves.append(Directions.WEST)
+
     # y-axis move
     if NORTH_DISTANCE >= 0 and Directions.NORTH in legal:
         bestMoves.append(Directions.NORTH)
+
     elif NORTH_DISTANCE <= 0 and Directions.SOUTH in legal:
         bestMoves.append(Directions.SOUTH)
+
 
     # print "EAST_D: ", EAST_DISTANCE, "  NORTH_D: ", NORTH_DISTANCE
 
@@ -328,7 +354,6 @@ def getFullWallAndPerimeterFromWallBlock(block, walls):
         for j in [-1, 0, 1]:
             surroundings.append((i, j))
     surroundings.remove((0, 0))
-    surroundings = np.array(surroundings)
     
     perimeter = set()
     knownWall = set()
@@ -397,12 +422,33 @@ def getPathsAroundTheWall(startPosition, goalPosition, perimeter):
             return path1, path2 + perimeter
         
 
-
 def sumTuples(tuple1, tuple2):
     return tuple([(i + j) for i, j in zip(tuple1, tuple2)])
 
-def getMapWidthAndHeight(state):
-    api
+
+def getVisitableCorners(corners):
+    topRightCorner = max(corners)
+    visitableCornerns = [(0,0), topRightCorner]
+
+
+def getVisitbaleWidthAndHeight(state):
+    topRightCorner = max(api.walls(state))
+
+    # the corners belong to the walls, so in a map 4 x 4
+    # the top right corner is (3, 3), but it's a wall, so the map is
+    # 2 x 2 from the agent perspective, so the width is 3 - 1
+    return (topRightCorner[0] - 1, topRightCorner[1] - 1)
+
+def getMapCellsMatrix(mapWidth, mapHeight):
+    widthArray = range(1, mapWidth+1)
+    heightArray = range(1, mapHeight+1)
+    return [element for element in itertools.product(heightArray, widthArray)]
+
+def getNonWallCells(walls, state):
+    width, height = getVisitbaleWidthAndHeight(state)
+    mapCellsMatrix = getMapCellsMatrix(height, width)
+    return [x for x in mapCellsMatrix if x not in walls]
+
 
 
 
@@ -423,17 +469,18 @@ class CornerSeekingAgent(Agent):
         }
         self.lastBestFoodPosition = None
         self.lastMinimumDistance = None
-        self.visitedCells = dict()
-
+        self.unvisitedCells = None
+        self.unvisitedCorners = None
+        self.walls = []
+        self.firstIteration = True
 
     def getAction(self, state):
-        if not self.unvisitedCorners:
+        if self.firstIteration:
+            self.firstIteration = False
             self.unvisitedCorners = api.corners(state)
-            mapWidthAndHeight = getMapWidthAndHeight(state)
-            self.unvisitedCells = np.arange()
-            
-        
-        print self.unvisitedCells
+            self.walls = api.walls(state)
+            self.unvisitedCells = getNonWallCells(self.walls, state)
+
 
         # What are the current moves available
         legal = api.legalActions(state)
@@ -442,7 +489,15 @@ class CornerSeekingAgent(Agent):
 
         # # Where is Pacman?
         pacman = api.whereAmI(state)
-        self.visitedCells.add(pacman)
+
+        # Update lists based on pacman new position
+        if pacman in self.unvisitedCells: self.unvisitedCells.remove(pacman)
+        if pacman == self.lastBestFoodPosition: self.lastBestFoodPosition = None
+        for corner in self.unvisitedCorners:
+            distanceToCorner = manhattanNotAbsoluteDistance(pacman, corner)
+            if (distanceToCorner[0] + distanceToCorner[1]) == 2: 
+                self.unvisitedCorners.remove(corner)
+            print "distance to corners", manhattanNotAbsoluteDistance(pacman, corner)
 
         # # Where are the ghosts?
         theGhosts = api.ghosts(state)
@@ -458,13 +513,19 @@ class CornerSeekingAgent(Agent):
         
         # # Where is the food?
         food = api.food(state)
-        print food
+        if len(food) == 0:
+            food = self.unvisitedCells
+        
+        ## TODO Remove this
+        food = self.unvisitedCorners
+
         foodDistances = [(util.manhattanDistance(pacman, point), index) for index, point in enumerate(food)]
         sortedFoodDistances = sorted(foodDistances)
+        print "SORTED TARGETS: ", sortedFoodDistances
         DISTANCE = 0
         FOOD_INDEX = 1
+
         firstFood = sortedFoodDistances[0]
-        secondFood = sortedFoodDistances[1]
         closestFood = food[firstFood[FOOD_INDEX]]
 
         # if firstFood[DISTANCE] == secondFood[DISTANCE]:
@@ -474,21 +535,22 @@ class CornerSeekingAgent(Agent):
 
         # if new closest food is not closer than the previous one
         # swipe it with the saved closest food
-        if (self.lastMinimumDistance and
-           sum(xyDistanceToClosestFood) > 1 and
-           pacman != self.lastBestFoodPosition):
+        # TODO UNCOMMENT for newer versions
+        # if (self.lastMinimumDistance and
+        #    sum(xyDistanceToClosestFood) > 1 and
+        #    pacman != self.lastBestFoodPosition):
 
-            if sum(xyDistanceToClosestFood) > sum(self.lastMinimumDistance):
-                print "Last Min Distance: ", self.lastMinimumDistance
-                print "Last Best Food: ", self.lastBestFoodPosition
-                print "ActualDistToClosestFood: ", xyDistanceToClosestFood
+        #     if sum(xyDistanceToClosestFood) > sum(self.lastMinimumDistance):
+        #         print "Last Min Distance: ", self.lastMinimumDistance
+        #         print "Last Best Food: ", self.lastBestFoodPosition
+        #         print "ActualDistToClosestFood: ", xyDistanceToClosestFood
 
-                closestFood = self.lastBestFoodPosition
-                print "CLOSEST_FOOD: ", closestFood
-                xyDistanceToClosestFood = manhattanNotAbsoluteDistance(closestFood, pacman)
+        #         closestFood = self.lastBestFoodPosition
+        #         print "CLOSEST_FOOD: ", closestFood
+        #         xyDistanceToClosestFood = manhattanNotAbsoluteDistance(closestFood, pacman)
             
-            elif sum(xyDistanceToClosestFood) == sum(self.lastMinimumDistance):
-                pass
+        #     elif sum(xyDistanceToClosestFood) == sum(self.lastMinimumDistance):
+        #         pass
 
         # update pacman knowledge
         self.lastMinimumDistance = xyDistanceToClosestFood
@@ -496,7 +558,12 @@ class CornerSeekingAgent(Agent):
 
         print " PAC: ", pacman, "CLOSEST FOOD: ", closestFood, "  DISTANCE FROM IT: ", xyDistanceToClosestFood
 
-        bestMoves = getBestNextMoves(legal, xyDistanceToClosestFood)
+        legalCopy = legal
+        if self.lastPosition in legalCopy:
+            legalCopy.remove(self.lastPosition)
+            print "REMOVED LAST POSITION: ", self.lastPosition
+
+        bestMoves = getBestNextMoves(legalCopy, xyDistanceToClosestFood)
         
         # print "Food locations: "
         # print food
@@ -512,17 +579,153 @@ class CornerSeekingAgent(Agent):
         # avoid ghost traplastPosition TODO
         # if self.lastPosition in bestMoves:
         #     return api.makeMove(self.lastPosition, legal)
+        if len(bestMoves) == 0:
+            bestMoves = legal
 
         # prevent coming back (AVOID LOOP)
+        print self.lastPosition
         if self.lastPosition in bestMoves and len(bestMoves) > 1:
             bestMoves.remove(self.lastPosition)
             print "REMOVED LAST POSITION: ", self.lastPosition
             print "NEW BEST MOVES: ", bestMoves
 
 
+        pick = random.choice(bestMoves)
+        self.lastPosition = self.oppositeDirections[pick]
+        print "SELECTED MOVE: ", pick
+        raw_input()
+        print "\n====================================\n"
+        return api.makeMove(pick, legal)
+
+
+class BetterHungryAgent(Agent):
+
+    def __init__(self):
+        self.lastPosition = Directions.STOP
+        self.oppositeDirections = {
+            Directions.SOUTH: Directions.NORTH,
+            Directions.NORTH: Directions.SOUTH,
+            Directions.EAST: Directions.WEST,
+            Directions.WEST: Directions.EAST     
+        }
+        self.lastBestFoodPosition = None
+        self.lastMinimumDistance = None
+        self.unvisitedCells = None
+        self.unvisitedCorners = None
+        self.walls = []
+        self.firstIteration = True
+
+    def getAction(self, state):
+        if self.firstIteration:
+            self.firstIteration = False
+            self.unvisitedCorners = api.corners(state)
+            self.walls = api.walls(state)
+            self.unvisitedCells = getNonWallCells(self.walls, state)
+
+
+        # # How far away are the ghosts?
+        # print "Distance to ghosts:"
+        # for i in range(len(theGhosts)):
+        #     print util.manhattanDistance(pacman,theGhosts[i])
+
+        # # Where are the capsules?
+        # print "Capsule locations:"
+        # print api.capsules(state)
+
+        # AVAILABLE MOVES
+        legal = api.legalActions(state)
+        if Directions.STOP in legal:
+            legal.remove(Directions.STOP)
+
+        # PACMAN Location
+        pacman = api.whereAmI(state)
+
+        # UPDATE LISTS BASED ON NEW PACMAN POSITION
+        if pacman in self.unvisitedCells: self.unvisitedCells.remove(pacman)
+        if pacman == self.lastBestFoodPosition: self.lastBestFoodPosition = None
+
+        # GHOSTS Location
+        theGhosts = api.ghosts(state)
+        
+        # FOOD Location
+        food = api.food(state)
+
+        # NO MORE FOOD, look for unvisited locations
+        if len(food) == 0:
+            print "NO FOOD FOUND, searching for unvisited cells..."
+            food = self.unvisitedCells
+
+        # CLOSEST TARGET
+        foodDistances = [(util.manhattanDistance(pacman, point), index) for index, point in enumerate(food)]
+        sortedFoodDistances = sorted(foodDistances)
+        FOOD_INDEX = 1
+
+        firstFood = sortedFoodDistances[0]
+        closestFood = food[firstFood[FOOD_INDEX]]
+        xyDistanceToClosestFood = manhattanNotAbsoluteDistance(closestFood, pacman)
+
+        # if new closest food is not closer than the previous one
+        # swipe it with the saved closest food
+        # TODO UNCOMMENT for newer versions
+        # if (self.lastMinimumDistance and
+        #    sum(xyDistanceToClosestFood) > 1 and
+        #    pacman != self.lastBestFoodPosition):
+
+        #     if sum(xyDistanceToClosestFood) > sum(self.lastMinimumDistance):
+        #         print "Last Min Distance: ", self.lastMinimumDistance
+        #         print "Last Best Food: ", self.lastBestFoodPosition
+        #         print "ActualDistToClosestFood: ", xyDistanceToClosestFood
+
+        #         closestFood = self.lastBestFoodPosition
+        #         print "CLOSEST_FOOD: ", closestFood
+        #         xyDistanceToClosestFood = manhattanNotAbsoluteDistance(closestFood, pacman)
+            
+        #     elif sum(xyDistanceToClosestFood) == sum(self.lastMinimumDistance):
+        #         pass
+
+        # UPDATE PACMAN TARGET Data
+        self.lastMinimumDistance = xyDistanceToClosestFood
+        self.lastBestFoodPosition = closestFood
+
+        # CALCULATE BEST MOVES
+        bestMoves = getBestNextMoves(legal, xyDistanceToClosestFood, immediateBestMoveFlag=True, maxDistanceForImmediateBestMove=3)
+
+        print "PAC: ", pacman, "CLOSEST TARGET: ", closestFood, "  DISTANCE FROM IT: ", xyDistanceToClosestFood
+        print "LAST POSITION: ", self.lastPosition
+        print "LEGAL: ", legal
+        print "BEST MOVES:", bestMoves
+
+        # AVOID LOOP
+        # avoid ghost traplastPosition TODO
+        # if self.lastPosition in bestMoves:
+        #     return api.makeMove(self.lastPosition, legal)
+
+        # NO BEST MOVE, REVERT TO ALL POSSIBLE
+        if len(bestMoves) == 0:
+            bestMoves = legal
+        else:
+            
+            # IMMEDIATE BEST MOVE
+            if bestMoves[-1] is True:
+                bestMoves.pop(-1)
+            else:
+                # best move takes more than 3 steps, so
+                # prevent coming back (AVOID LOOP)
+                if self.lastPosition in bestMoves:
+                    if len(bestMoves) > 1:
+                        print "== OLD BEST MOVES IS ====> ", bestMoves
+                        bestMoves.remove(self.lastPosition)
+                        print "== REMOVED LAST POSITION: ", self.lastPosition
+                        print "== NEW BEST MOVES: ", bestMoves
+                    else:
+                        if len(legal) > 1:
+                            legal.remove(self.lastPosition)
+                            bestMoves = legal
+
 
         pick = random.choice(bestMoves)
         self.lastPosition = self.oppositeDirections[pick]
+        print "SELECTED MOVE: ", pick
         raw_input()
         print "\n====================================\n"
         return api.makeMove(pick, legal)
